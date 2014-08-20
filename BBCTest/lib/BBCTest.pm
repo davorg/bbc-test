34778a238fd49c41c0453a95a50c65897c73ee5e
Dave Cross <dave@dave.org.uk>
2014-08-20 18:41:10 +0100

    Return nasty hand-made XML

--- BBCTest/lib/BBCTest.pm
+++ BBCTest/lib/BBCTest.pm
@@ -1,5 +1,6 @@
 package BBCTest;
 use Dancer ':syntax';
+use DateTime;
 
 our $VERSION = '0.1';
 
@@ -8,8 +9,15 @@ get '/' => sub {
 };
 
 get '/video' => sub {
+    content_type => 'application/xml';
     my $id = param('id');
-    return $id;
+    my $start = DateTime->now;
+    my $end = $start->clone->add({ seconds => 3600 });
+    return qq[<vidauth sig="yyyyy">
+  <id>$id</id>
+  <authid>xxxxx</authid>
+  <available start="$start" end="$end"/>
+</vidauth>];
 };
 
 true;
